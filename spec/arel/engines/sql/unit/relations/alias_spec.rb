33e36e55cdfaa561ab870115dac2c9277b6399a6
Nick Kallen <nkallen@nick-kallens-computer-2.local>
2008-05-26 21:02:23 -0700

    reorganizing tests

---
+++ spec/arel/engines/sql/unit/relations/alias_spec.rb
@@ -0,0 +1,36 @@
+require File.join(File.dirname(__FILE__), '..', '..', '..', 'spec_helper')
+
+module Arel
+  describe Alias do
+    before do
+      @relation = Table.new(:users)
+    end
+    
+    describe '==' do
+      it "obtains if the objects are the same" do
+        Alias.new(@relation).should_not == Alias.new(@relation)
+        (aliaz = Alias.new(@relation)).should == aliaz
+      end
+    end
+    
+    describe '#to_sql' do
+      describe 'when there is no ambiguity' do
+        it 'does not alias table names anywhere a table name can appear' do
+          @relation                       \
+            .where(@relation[:id].eq(1))  \
+            .order(@relation[:id])        \
+            .project(@relation[:id])      \
+            .group(@relation[:id])        \
+            .alias                        \
+          .to_sql.should be_like("
+            SELECT `users`.`id`
+            FROM `users`
+            WHERE `users`.`id` = 1
+            ORDER BY `users`.`id` ASC
+            GROUP BY `users`.`id`
+          ")
+        end
+      end
+    end
+  end
+end
---
+++ spec/arel/engines/sql/unit/relations/delete_spec.rb
@@ -0,0 +1,34 @@
+require File.join(File.dirname(__FILE__), '..', '..', '..', 'spec_helper')
+
+module Arel
+  describe Deletion do
+    before do
+      @relation = Table.new(:users)
+    end
+  
+    describe '#to_sql' do
+      it 'manufactures sql deleting a table relation' do
+        Deletion.new(@relation).to_sql.should be_like("
+          DELETE
+          FROM `users`
+        ")
+      end
+    
+      it 'manufactures sql deleting a where relation' do
+        Deletion.new(@relation.where(@relation[:id].eq(1))).to_sql.should be_like("
+          DELETE
+          FROM `users`
+          WHERE `users`.`id` = 1
+        ")
+      end
+      
+      it "manufactures sql deleting a ranged relation" do
+        Deletion.new(@relation.take(1)).to_sql.should be_like("
+          DELETE
+          FROM `users`
+          LIMIT 1
+        ")
+      end
+    end
+  end
+end
