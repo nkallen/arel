32822b4b3f5bcd61d4fc915ad7670a1e81069abe
Nick Kallen <nkallen@nick-kallens-computer-2.local>
2008-05-27 18:01:28 -0700

    joining across engines in either direction

--- spec/arel/engines/memory/integration/joins/cross_engine_spec.rb
+++ spec/arel/engines/memory/integration/joins/cross_engine_spec.rb
@@ -30,17 +30,34 @@ module Arel
         .insert(@relation2[:id] => 1, @relation2[:bird_id] => 1, @relation2[:color] => 'yellow')  \
         .insert(@relation2[:id] => 2, @relation2[:bird_id] => 2, @relation2[:color] => 'gray')
     end
+
+    describe 'when the in memory relation is on the left' do
+      it 'joins across engines' do
+        @relation1                                              \
+          .join(@relation2)                                     \
+            .on(@relation1[:id].eq(@relation2[:bird_id]))       \
+          .project(@relation1[:name], @relation2[:color])       \
+        .let do |relation|
+          relation.call.should == [
+            Row.new(relation, ['duck', 'yellow']),
+            Row.new(relation, ['pigeon', 'gray'])
+          ]
+        end
+      end
+    end
     
-    it 'joins across engines' do
-      @relation1                                              \
-        .join(@relation2)                                     \
-          .on(@relation1[:id].eq(@relation2[:bird_id]))       \
-        .project(@relation1[:name], @relation2[:color])       \
-      .let do |relation|
-        relation.call.should == [
-          Row.new(relation, ['duck', 'yellow']),
-          Row.new(relation, ['pigeon', 'gray'])
-        ]
+    describe 'when the in memory relation is on the right' do
+      it 'joins across engines' do
+        @relation2                                              \
+          .join(@relation1)                                     \
+            .on(@relation1[:id].eq(@relation2[:bird_id]))       \
+          .project(@relation1[:name], @relation2[:color])       \
+        .let do |relation|
+          relation.call.should == [
+            Row.new(relation, ['duck', 'yellow']),
+            Row.new(relation, ['pigeon', 'gray'])
+          ]
+        end
       end
     end
   end
